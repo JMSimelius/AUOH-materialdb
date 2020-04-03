43585a64d2da03fa7990a84284ca68bba180965c
Juha-Matti Simelius <juha-matti.simelius@seamk.fi>
2020-04-03 18:33:38 +0300

    ttt

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const port = process.env.PORT || 8080;
+const app = express();
+
+const body_parser = require('body-parser');
+
+const material_controller=require('./material_controller');
+
+app.use(body_parser.json()); //req.body.name
+app.use(body_parser.urlencoded({
+    extended:true
+})); // material/id
+
+app.use(  (req, res, next)=>{
+    console.log(req.method, ' ', req.path);
+    next();
+}); // GET /api/materials
+
+// RESTful API
+// CRUDS OPERATIONS
+
+// CREATE
+
+// READ
+app.get("/api/materials", material_controller.api_get_materials);
+// UPDATE
+
+// DELETE
+
+
+app.listen(port);
